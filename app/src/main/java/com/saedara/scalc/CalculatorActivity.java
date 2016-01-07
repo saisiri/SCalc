9c2a9901bb181e25e7acb9798094a30763ffc8e6
sai <saedara@ebay.com>
2016-01-07 16:08:16 +0100

    Rename

--- app/src/main/java/com/saedara/scalc/CalculatorActivity.java
+++ app/src/main/java/com/saedara/scalc/CalculatorActivity.java
@@ -1,13 +1,10 @@
 package com.saedara.scalc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
@@ -21,9 +18,9 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 
 
-    public void calculatePercentage(View view) {
-        double amt = Double.parseDouble(((EditText) findViewById(R.id.amountEditText)).getText().toString());
-        double percentage = Double.parseDouble(((EditText) findViewById(R.id.percentEditText)).getText().toString());
+    public void calculate(View view) {
+        double amt = Double.parseDouble(((EditText) findViewById(R.id.op1EditText)).getText().toString());
+        double percentage = Double.parseDouble(((EditText) findViewById(R.id.op2EditText)).getText().toString());
 
         TextView resultView = (TextView)findViewById(R.id.resultTextView);
         resultView.setText(Double.toString(amt * percentage / 100));
