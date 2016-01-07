cefb188d1d39d281547ab2174e1f81855a1a537f
sai <saedara@ebay.com>
2016-01-07 17:49:36 +0100

    Improve calculator

--- app/src/main/java/com/saedara/scalc/CalculatorActivity.java
+++ app/src/main/java/com/saedara/scalc/CalculatorActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
@@ -15,14 +17,18 @@ public class CalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-    public void calculate(View view) {
-        double amt = Double.parseDouble(((EditText) findViewById(R.id.op1EditText)).getText().toString());
-        double percentage = Double.parseDouble(((EditText) findViewById(R.id.op2EditText)).getText().toString());
-
-        TextView resultView = (TextView)findViewById(R.id.resultTextView);
-        resultView.setText(Double.toString(amt * percentage / 100));
+        GridView operations = (GridView)findViewById(R.id.operationsGridView);
+        operations.setAdapter(new OperatorsAdapter(this));
+        operations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                double operand1 = Double.parseDouble(((EditText)findViewById(R.id.op1EditText)).getText().toString());
+                double operand2 = Double.parseDouble(((EditText)findViewById(R.id.op2EditText)).getText().toString());
+                String operator = ((TextView)view).getText().toString();
+                ((TextView)findViewById(R.id.resultTextView)).setText(operand1 + " " + operator + " " + operand2);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/saedara/scalc/OperatorsAdapter.java
@@ -0,0 +1,55 @@
+package com.saedara.scalc;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+/**
+ * Created by saedara on 1/7/16.
+ */
+public class OperatorsAdapter extends BaseAdapter {
+
+    private Context context;
+    private static String[] operators = {
+       "+", "-", "/", "*", "%", "pow", "sqrt"
+    };
+
+    public OperatorsAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView view;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            view = new TextView(context);
+            view.setText(operators[position]);
+            view.setLayoutParams(new GridView.LayoutParams(85, 85));
+            view.setPadding(5, 5, 5, 5);
+        } else {
+            view = (TextView)convertView;
+        }
+
+        return view;
+    }
+}
